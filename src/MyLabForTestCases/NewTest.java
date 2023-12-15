194fb3cf5ba5cc01acb618fc7cd47a087a1f448e
“Eason0731” <Eason.Zhang0731@outlook.com>
2023-12-15 16:17:32 +0800

    Update Selenium version from v4.10.0 to v4.16.0 and also renew some other files

---
+++ src/MyLabForTestCases/NewTest.java
@@ -0,0 +1,30 @@
+package MyLabForTestCases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import BrowserDrivers.GetBrowserDriver;
+
+public class NewTest {
+	WebDriver driver;
+  @Test
+  public void CheckOnBaidu() throws Exception {
+	  String Baidu = "http://www.baidu.com";
+	  driver.navigate().to(Baidu);
+	  Thread.sleep(3000);
+	  Assert.assertEquals(driver.getTitle(), "百度一下，你就知道");
+	  
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  driver = GetBrowserDriver.GetChromeDriver();
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  driver.quit();
+  }
+
+}
